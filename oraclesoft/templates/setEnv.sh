23f3ac6cf03cdce1d2c0c1ec4e88d0139248b9f9
patinazo <fernando.tamariz@gmail.com>
2020-01-13 14:31:32 +0000

    Añadido variable entorno

--- oraclesoft/templates/setEnv.sh
+++
@@ -1,8 +0,0 @@
-# Oracle Settings
-export TMP=/tmp
-export TMPDIR=\$TMP
-export ORACLE_BASE={{ oracle_base }}
-export ORACLE_HOME={{ oracleclient_home }}
-#export ORACLE_HOME=\$ORACLE_BASE/product/19.0.0/client_1
-export PATH=\$ORACLE_HOME/bin:\$PATH
-export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
